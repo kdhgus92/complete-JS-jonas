fba7b636209cca707c0dbf9fba8254f0a25ef2fb
Ohhyeun Kwon <kdhgus92@gmail.com>
2021-02-20 23:57:47 +0900

    141 of 314

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -329,6 +329,9 @@ booker();
 console.dir(booker);
 */
 
+/*
+// 137. More Closuer Examples
+
 // Example 1
 let f;
 
@@ -374,4 +377,31 @@ const boardPassengers = function (n, wait) {
 // }, 1000);
 
 const perGroup = 1000;
+// Closure의 우선순위
 boardPassengers(180, 3);
+*/
+
+/*
+// Coding Challenge #2 
+
+This is more of a thinking challenge than a coding challenge 🤓
+
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
