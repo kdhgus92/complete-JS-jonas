13bcfaff1eebd22f5e42c8a501d80042e0038444
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-07-26 14:24:15 +0900

    289. npm

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -94,3 +94,19 @@ export.addTocart = function (product, quantity) {
 // Import 
 const {addTocart} = require('./shoppingCart.js');
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone); // clone된 녀석도 바뀐다.
+console.log(stateDeepClone); // 독립적
