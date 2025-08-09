1af73ec93f1acbbc2896b179166e4759037c0866
Ohhyeun Kwon <35871896+kdhgus92@users.noreply.github.com>
2025-08-09 19:07:11 +0900

    321. upload recipe

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a568e2f4-c43a-4f0b-828b-902411e0ad88';
+export const MODAL_CLOSE_SEC = 2.5;
